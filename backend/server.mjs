ffef129b3166a264ebb1195159359e21a4aa2c3f
HGhulamHamza <khawajahamza566@gmail.com>
2024-10-11 14:14:32 +0500

    Route for DELETING all the users done

--- backend/server.mjs
+++ backend/server.mjs
@@ -79,6 +79,22 @@ app.put('/api/users/:id', async (req, res) => {
   });
   
 
+  // DELETE a user
+app.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deletedUser = await User.findByIdAndDelete(id);
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found!' });
+      }
+      res.status(200).json({ message: 'User deleted successfully!' });
+    } catch (err) {
+      res.status(500).json({ message: 'Server Error', error: true });
+    }
+  });
+  
+
 
 
 
